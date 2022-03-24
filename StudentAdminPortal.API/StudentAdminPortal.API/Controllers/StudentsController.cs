5b3970eec91f857851c22bacfda39558ba77b875
baldrick615 <tmcgrath28@gmail.com>
2022-03-24 12:12:58 -0500

    Adding automapper functionality, making methods Async

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Controllers/StudentsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentAdminPortal.API.Repositories;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using StudentAdminPortal.API.DomainModels;
+using AutoMapper;
+
+namespace StudentAdminPortal.API.Controllers
+{
+    [ApiController]
+    public class StudentsController : Controller
+    {
+        private readonly IStudentRepository studentRepository;
+        private readonly IMapper mapper;
+        public StudentsController(IStudentRepository studentRepository, IMapper mapper)
+        {
+            this.studentRepository = studentRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        [Route("[controller]")]
+        public async Task<IActionResult> GetAllStudentsAsync()
+        {
+            var students = await studentRepository.GetStudentsAsync();
+            
+            mapper.Map<List<Student>>(students);
+
+            return Ok(mapper.Map<List<Student>>(students));
+        }
+    }
+}
---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Repositories/SqlStudentRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using StudentAdminPortal.API.DataModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortal.API.Repositories
+{
+    public class SqlStudentRepository : IStudentRepository
+    {
+        private readonly StudentAdminContext context;
+
+        public SqlStudentRepository(StudentAdminContext context)
+        {
+            this.context = context;
+        }
+        public async Task<List<Student>> GetStudentsAsync()
+        {
+            return await context.Student.Include(nameof(Gender)).Include(nameof(Address)).ToListAsync();
+        }
+    }
+}
