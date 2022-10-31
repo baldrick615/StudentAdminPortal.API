56f5d9d788736b994111cf254284bd0664ac530f
baldrick615 <tmcgrath28@gmail.com>
2022-10-30 21:09:18 -0500

    End Of Section 2 - API

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Profiles/AutoMapperProfiles.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using StudentAdminPortal.API.DataModels;
+using DataModels = StudentAdminPortal.API.DomainModels;
+
+
+namespace StudentAdminPortal.API.Profiles
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<DataModels.Student, DomainModels.Student>().ReverseMap();
+            CreateMap<DataModels.Address, DomainModels.Address>().ReverseMap();
+            CreateMap<DataModels.Gender, DomainModels.Gender>().ReverseMap();
+        }
+    }
+}
--- StudentAdminPortal.API/StudentAdminPortal.API/Program.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Program.cs
@@ -1,11 +1,28 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using StudentAdminPortal.API.DataModels;
+using StudentAdminPortal.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("StudentAdminPortalDb");
+builder.Services.AddDbContext<StudentAdminContext>(options => options.UseSqlServer(connectionString));
+//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IStudentRepository, SqlStudentRepository>();
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//AutoMapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 
 var app = builder.Build();
 
